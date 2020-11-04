5862a1bbbbd0068f8da786abb7de7adb6a0b680c
Ryuichi <ne191008@senshu-u.jp>
2020-11-04 00:54:52 +0000

    バリテーション

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,4 +2,5 @@ class Friend < ApplicationRecord
 
     has_many :telephones
 
+    validates :name, presence:true, length:{maximum:10}
 end
--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -2,4 +2,9 @@ class Telephone < ApplicationRecord
 
   belongs_to :friend, class_name: "Friend", foreign_key: "friend_id"
 
+  validates :number, presence:true, 
+    format:{with: /\A[0-9()-][0-9()-]*\z/, allow_blank:false},
+    length:{minimum:8, maximum:20},
+    uniqueness: {case_sensitive:false}
+
 end
